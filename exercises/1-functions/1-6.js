6da0cb7beb72cc56a6e8361605460d48b1ef8dd4
Kelly Van der Stock <kelly.van.der.stock@student.ehb.be>
2023-10-13 13:37:37 +0200

    1-6 Sunset - Ellipse

---
+++ exercises/1-functions/1-6.js
@@ -0,0 +1,23 @@
+"use strict";
+
+window.onresize = drawEllipse;
+
+drawEllipse();
+
+function drawEllipse() {
+	let canvas = document.querySelector("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let context = canvas.getContext("2d");
+
+	context.fillStyle = "#fea500";
+	context.fillRect(50, 50, 300, 300);
+
+	context.fillStyle = "#ffff01";
+	context.beginPath();
+	context.ellipse(200, 250, 50, 100, Math.PI /2, 0, 2 * Math.PI);
+	context.fill();
+
+	context.fillStyle = "#0000FE";
+	context.fillRect(50, 250, 300, 100);
+}
