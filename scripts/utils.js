ed5edbc2c91cfaba55088f25fdd759385e22e3d5
Kelly Van der Stock <kelly.van.der.stock@student.ehb.be>
2023-11-08 11:43:21 +0100

    3-1-a drawLines

--- scripts/utils.js
+++ scripts/utils.js
@@ -12,3 +12,17 @@ export function fillCircle(x, y, r) {
 	context.arc(x, y, r, 0, Math.PI * 2);
 	context.fill();
 }
+
+/**
+ * draw a line between 2 coordinates
+ * @param {number} x1 startpunt op x van de lijn 
+ * @param {number} y1 startpunt op y van de lijn 
+ * @param {number} x2 eindpunt op x van de lijn
+ * @param {number} y2 eindpunt op x van de lijn
+ */
+export function drawLine(x1,y1,x2,y2){
+    context.beginPath();
+    context.moveTo (x1,y1);
+    context.lineTo(x2,y2);
+    context.stroke();
+}
