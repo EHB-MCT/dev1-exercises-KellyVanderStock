85eef1ff97bcdabff42149b590f9db737945a4c3
Kelly Van der Stock <kelly.van.der.stock@student.ehb.be>
2023-11-18 11:03:31 +0100

    4-1 more lines

---
+++ exercises/4-iteration-nesting-variation/4-1-More-Lines.js
@@ -0,0 +1,32 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+
+draw();
+
+function draw() {
+    context.lineWidth = 2;
+    context.strokeStyle = "#fff";
+    context.fillStyle = "orange";
+    context.fillRect(0, 0, 600, 300);
+    drawMoreLines();
+}
+
+
+
+function drawMoreLines(){
+
+   
+  for( let x=0; x<=60;x++){
+    Utils.drawLine(0+x*10,0,600-x*10,300 );
+
+  }
+
+
+for( let y=0; y<=30;y++){
+    Utils.drawLine(0,0+y*10,600,300-y*10 )
+}
+
+}
+
